42093b13b0eedc0e6c3e89dbabf06a81f2cb6ecd
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-05 22:53:41 +0530

    Update'

--- Doc/conf.py
+++ Doc/conf.py
@@ -1,16 +1,25 @@
+# conf.py
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Your Project'
+author = 'Your Name'
+release = '1.0.0'
+
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "sphinx_multiversion"
+    'sphinx_multiversion',
+    'myst_parser',  # only if you use Markdown files
 ]
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-html_context = {
-    'display_github': True,
-}
+html_theme = 'sphinx_rtd_theme'  # or 'furo', etc.
 
-smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
-smv_branch_whitelist = r'^master$'
-smv_latest_version = 'master'
+# Sphinx-multiversion config
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'   # tags like v1.0.0
+smv_branch_whitelist = r'^(main|master)$'  # main or master branch
+smv_latest_version = 'master'  # what branch/tag to treat as latest
+smv_rename_latest_version = 'latest'  # rename 'main' folder to 'latest' in output

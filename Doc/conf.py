98467b841a12130d77c16e840ff9535d29c0e28f
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-05 23:07:37 +0530

    Update'

--- Doc/conf.py
+++ Doc/conf.py
@@ -18,8 +18,16 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'  # or 'furo', etc.
 
-# Sphinx-multiversion config
-smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'   # tags like v1.0.0
-smv_branch_whitelist = r'^(main|master)$'  # main or master branch
-smv_latest_version = 'master'  # what branch/tag to treat as latest
-smv_rename_latest_version = 'latest'  # rename 'main' folder to 'latest' in output
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'    # build tags like v1.0.0
+smv_branch_whitelist = r'^master$'          # build only master branch for latest
+smv_latest_version = 'master'                # treat master branch as latest
+smv_rename_latest_version = 'latest'
+
+html_sidebars = {
+    '**': [
+        'version-switch.html',
+        'globaltoc.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}

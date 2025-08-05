a8e4c2c537b854c4b73b1cf532eccbdf0ff8da24
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-05 22:41:29 +0530

    Update'

--- Doc/conf.py
+++ Doc/conf.py
@@ -1,39 +1,15 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-
-project = 'Your Project Name'
-author = 'Your Name'
-release = 'x.x.x'  # Match this with git tag
-
 extensions = [
-    'myst_parser',              # for Markdown
-    'sphinx_rtd_theme',         # for nice layout
-    'sphinx_multiversion',      # for versioned docs
+    'sphinx_multiversion',
+    'myst_parser',  # only if you're using Markdown (.md) files
+    ...
 ]
-master_doc = 'index'
-
-html_theme = 'sphinx_rtd_theme'
-master_doc = 'index'
-# Versioning Config
-smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
-smv_branch_whitelist = r'^master$'
-smv_remote_whitelist = r'^origin$'
-smv_released_pattern = r'^tags/v\d+\.\d+\.\d+$'
-
-html_context = {
-    'display_github': True,
-    'versions': [],
-}
 
 templates_path = ['_templates']
 
-html_sidebars = {
-    '**': [
-        'version-switch.html',   # ← Add this line
-        'globaltoc.html',
-        'relations.html',
-        'sourcelink.html',
-        'searchbox.html',
-    ],
+html_context = {
+    'display_github': True,
 }
+
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
+smv_branch_whitelist = r'^master$'
+smv_latest_version = 'main'

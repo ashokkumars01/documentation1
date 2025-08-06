6462d1a006704db7e3277aa7ddd05986b1b9cdb3
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-06 17:38:47 +0530

    Update

--- Doc/conf.py
+++ Doc/conf.py
@@ -40,14 +40,25 @@
 #     ]
 # }
 
+# conf.py
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'YourProject'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-templates_path = ['_templates']
-html_sidebars = {
-    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+project = 'Your Project'
+html_theme = 'alabaster'  # or sphinx_rtd_theme, furo, etc.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_multiversion',
+]
+
+# sphinx-multiversion config
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'  # Only tags like v1.0.0
+smv_branch_whitelist = r'^master$'        # Only build from `main`
+smv_remote_whitelist = r'^origin$'      # Only from origin
+
+# Where to store versions.json
+html_context = {
+    'display_github': True,
+    'versions_json_url': '/versions.json'
 }

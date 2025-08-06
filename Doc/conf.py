99d82605a2423fa13955fcc45ca3afa7214fc6f6
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-06 16:29:59 +0530

    update

--- Doc/conf.py
+++ Doc/conf.py
@@ -1,42 +1,53 @@
 # conf.py
 
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# project = 'Your Project'
+# author = 'Your Name'
+# release = '1.0.0'
+
+# extensions = [
+#     'sphinx_multiversion',
+#     'myst_parser',  # only if you use Markdown files
+#     'sphinx.ext.autodoc'
+# ]
+
+# html_theme = "sphinx_rtd_theme"
+# html_context = {
+#     'display_github': True,
+# }
+# templates_path = ['Doc/_templates', 'Doc/_static']
+# html_js_files = ['versions-menu.js']
+# exclude_patterns = []
+
+# html_theme = 'sphinx_rtd_theme'  # or 'furo', etc.
+
+# smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'    # build tags like v1.0.0
+# smv_branch_whitelist = r'^master$'          # build only master branch for latest
+# smv_latest_version = 'master'                # treat master branch as latest
+# smv_rename_latest_version = 'latest'
+
+# html_sidebars = {
+#     '**': [
+#         'layout.html',
+#         'version-switch.html',
+#         'globaltoc.html',
+#         'relations.html',
+#         'searchbox.html',
+        
+#     ]
+# }
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Your Project'
-author = 'Your Name'
-release = '1.0.0'
-
-extensions = [
-    'sphinx_multiversion',
-    'myst_parser',  # only if you use Markdown files
-    'sphinx.ext.autodoc'
-]
-
-html_theme = "sphinx_rtd_theme"
-html_context = {
-    'display_github': True,
-}
-templates_path = ['Doc/_templates', 'Doc/_static']
-html_js_files = ['versions-menu.js']
-exclude_patterns = []
-
-html_theme = 'sphinx_rtd_theme'  # or 'furo', etc.
-
-smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'    # build tags like v1.0.0
-smv_branch_whitelist = r'^master$'          # build only master branch for latest
-smv_latest_version = 'master'                # treat master branch as latest
-smv_rename_latest_version = 'latest'
-
+project = 'YourProject'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+templates_path = ['_templates']
 html_sidebars = {
-    '**': [
-        'layout.html',
-        'version-switch.html',
-        'globaltoc.html',
-        'relations.html',
-        'searchbox.html',
-        
-    ]
+    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
 }
-

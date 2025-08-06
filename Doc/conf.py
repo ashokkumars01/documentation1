fc08364afbf7b87af92cc2975c4221307df1a871
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-06 18:18:12 +0530

    Update

--- Doc/conf.py
+++ Doc/conf.py
@@ -52,6 +52,10 @@ extensions = [
     'sphinx_multiversion',
 ]
 templates_path = ['Doc/_templates']
+
+html_static_path = ['_static']
+html_css_files = ['version_switch.css']
+
 # sphinx-multiversion config
 smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'  # Only tags like v1.0.0
 smv_branch_whitelist = r'^master$'        # Only build from `main`
